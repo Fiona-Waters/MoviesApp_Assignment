e86385f95336f577fa94580025ae85e4b9eea25a
Fiona Waters <fiwaters6@gmail.com>
2022-06-30 21:15:15 +0100

    New button for upcoming movies page

---
+++ src/components/cardIcons/playlistAddIcon.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PlaylistAddIcon from "@material-ui/icons/PlaylistAdd";
+
+const MyPlaylistAddIcon = () => {
+    return (
+        <PlaylistAddIcon/>
+    )
+};
+
+export default MyPlaylistAddIcon;
